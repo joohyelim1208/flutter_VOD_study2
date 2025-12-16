a7573769466934026a4a091cc9be0ab15773ab62
joohyelim1208 <jhl01208@gmail.com>
2025-12-15 21:58:16 -0800

    creat_widjet_file_3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app/recipe_ltem.dart';
 import 'package:flutter_recipe_app/recipe_menu.dart';
@@ -25,7 +26,22 @@ class RecipePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // 3) 상단 앱 바를 생성하고 시작.
-      appBar: AppBar(),
+      appBar: AppBar(
+        // 5) 앱 바에 들어갈 아이콘 넣기
+        Icon(
+          CupertinoIcons.search, // 써치모양 아이콘
+          size: 30,
+        ),
+        SizedBox(width: 15),
+        Icon(
+          CupertinoIcons.heart, // 하트모양 아이콘
+          size: 30,
+          Color: Colors.red,
+          // 5) 아이콘들이 우측 상단에 배열되어 있음. SizedBox를 이용해 여백을 주자.
+        ),
+        // 5) 앱 바 구현 완료!! -> 레시피 타이틀 만들기.
+        SizedBox(width: 15),
+      ),
       // 3) 위젯이 들어갈 body를 만들어줌
       body: Column(
         // 4) 가로정렬을 써준다. 주축기준으로 왼쪽, 위쪽 / 교차쪽 start, end, center
