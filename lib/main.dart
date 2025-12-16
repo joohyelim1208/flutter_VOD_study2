82b63916aaf87467090f99f829df52980e385835
joohyelim1208 <jhl01208@gmail.com>
2025-12-15 23:45:05 -0800

    creat_widjet_file_6

--- lib/main.dart
+++ lib/main.dart
@@ -48,19 +48,25 @@ class RecipePage extends StatelessWidget {
         // 5) 앱 바 구현 완료!! -> 레시피 타이틀 만들기.
         SizedBox(width: 15),
       ),
-      // 3) 위젯이 들어갈 body를 만들어줌
-      body: Column(
-        // 4) 가로정렬을 써준다. 주축기준으로 왼쪽, 위쪽 / 교차쪽 start, end, center
-        crossAxisAlignment: CrossAxisAlignment.start, // 4) 뼈대 작성 완료
+      // 3) 위젯이 들어갈 body: Column()를 만들어줌
+      // 11) Column 커맨드+. 'Wrap widget Padding'
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ), // 11) 가로방향만 적용할거라 symmetric. 다시 레시피 메뉴로 돌아가서,
+        child: Column(
+          // 4) 가로정렬을 써준다. 주축기준으로 왼쪽, 위쪽 / 교차쪽 start, end, center
+          crossAxisAlignment: CrossAxisAlignment.start, // 4) 뼈대 작성 완료
 
-        children: [
-          RecipeTitle(),
-          RecipeMenu(),
-          RecipeListItem(),
-          RecipeListItem(),
-          RecipeListItem(), // 3) 각 위젯들이 잘 배치 되었는지 가상 디바이스로 확인.
-          // 3) 각각의 위젯이 text로 배치된 것을 확인. 좌측 가운데 정렬되어 있음
-        ],
+          children: [
+            RecipeTitle(),
+            RecipeMenu(),
+            RecipeListItem(),
+            RecipeListItem(),
+            RecipeListItem(), // 3) 각 위젯들이 잘 배치 되었는지 가상 디바이스로 확인.
+            // 3) 각각의 위젯이 text로 배치된 것을 확인. 좌측 가운데 정렬되어 있음
+          ],
+        ),
       ),
     );
   }
--- lib/recipe_menu.dart
+++ lib/recipe_menu.dart
@@ -4,35 +4,54 @@ class RecipeMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 5) 레시피 메뉴바 구성
-    return Row(
-      children: [
-        Container(
-          width: 60,
-          height: 80,
-          // 5) 아이콘에 테두리를 생성해줌
-          decoration: BoxDecoration(
-            // 5) color 속성에 커맨드+클릭. 들어가보면 각 색상에 대한 정보가 나옴 참고해서 조정하기
-            border: Border.all(color: Colors.black12),
-            // 5) 모서리를 둥글게
-            borderRadius: BorderRadius.circular(30),
-          ),
-
-          // 6) 아이콘과 텍스트가 세로로 배치되어 있는 상태에서
-          child: Column(
-            // 7) 가운데로 이미지와 텍스트 정렬해줌
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.food_bank,
-              size: 30,
-              color: Colors.redAccent,
-              )
-              Text('ALL'), // 6) 여기까지 아이콘의 색상, 사이즈, 들어가는 텍스트가 버튼 모양으로 구현됨
-              SizedBox(height: 5,) // 7) 글자와 아이콘의 간격이 조금 벌어져서 조정해줌. 아이콘 구현 완료!
-            ],
-          ),
-        ),
-      ],
+    // 11) Row도 패딩으로 감싸주기. 레시피 메뉴 구현 완료!!!
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Row(
+        // 8) Container 함수
+        children: [
+          // 10) menuItem()에 수정된 함수를 넘겨줄 파라미터를 만들기
+          // 10) 각각에 맞는 아이콘을 넣으면 정상 출력. 이제 간격을 조정해줘야 함
+          menuItem(Icons.food_bank, 'All'),
+          SizedBox(width: 25),
+          menuItem(Icons.emoji_food_beverage, 'Coffee'),
+          SizedBox(width: 25),
+          menuItem(Icons.fastfood, 'Burger'),
+          SizedBox(width: 25), // 10) 여기까지 간격은 조정됐지만 왼쪽에 위치해서 가운데로 Padding을 줘서 정렬해야 함. 해당 앱은 전부 동일한 정렬이라 main으로 가서 동일하게 적용.
+          menuItem(Icons.local_pizza, 'Pizza'),
+        ],
+      ),
     );
+  }
+
+  Container menuItem(IconData icon, String text) { // 9) menuItem() 함수 안에 들어가는 작업을 하기.
+    return Container( // 8) 커맨드+. 'Extract Method' 제목은 menuItem 이라고 해준다. Container가 별도의 함수로 빠짐!
+        width: 60,
+        height: 80,
+        // 5) 아이콘에 테두리를 생성해줌
+        decoration: BoxDecoration(
+          // 5) color 속성에 커맨드+클릭. 들어가보면 각 색상에 대한 정보가 나옴 참고해서 조정하기
+          border: Border.all(color: Colors.black12),
+          // 5) 모서리를 둥글게
+          borderRadius: BorderRadius.circular(30),
+        ),
+
+        // 6) 아이콘과 텍스트가 세로로 배치되어 있는 상태에서
+        child: Column(
+          // 7) 가운데로 이미지와 텍스트 정렬해줌
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Icon(Icons.food_bank, 9) 삭제하고 함수 수정
+            Icon(
+            size: 30,
+            color: Colors.redAccent,
+            )
+            // 9) 삭제하고 함수 수정. Text('ALL'), // 6) 여기까지 아이콘의 색상, 사이즈, 들어가는 텍스트가 버튼 모양으로 구현됨
+            Text(text),
+            SizedBox(height: 5,) // 7) 글자와 아이콘의 간격이 조금 벌어져서 조정해줌. 아이콘 구현 완료!
+          ],
+        ),
+      );
   }
 }
 // 8) 아이콘을 여러개 똑같이 구현할 때 Container를 복붙할 수도 있지만,
